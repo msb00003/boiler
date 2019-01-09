515ffa562b657e2b669dfdc462279143a3c63955
msb00003 <msb00003@gmail.com>
2019-01-09 21:23:38 +0000

    added relay

--- hardware/boiler.py
+++ hardware/boiler.py
@@ -1,7 +1,24 @@
-# TODO: these
+import os
+
+
 def set_boiler_high():
-    pass
+    return
 
 
 def set_boiler_low():
-    pass
+    return
+
+if not os.getenv("LOCAL"):
+    import RPi.GPIO as GPIO
+
+    RELAY_PIN = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+
+    def set_boiler_high():
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
+
+
+    def set_boiler_low():
+        GPIO.output(RELAY_PIN, GPIO.LOW)
